f283affae85d15175104a319fc70c93361dbd5fb
stsan9 <stsan@ucsd.edu>
2020-02-04 22:15:13 -0800

    now collects vespene

--- mybot.py
+++ mybot.py
@@ -1,7 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 
 class StevenBot(sc2.BotAI):
@@ -9,6 +9,7 @@ class StevenBot(sc2.BotAI):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
+        await self.build_assimilators()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -21,6 +22,18 @@ class StevenBot(sc2.BotAI):
             if nexuses.exists:
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near = nexuses.first)
+
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+            for vesp in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vesp.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vesp).exists:
+                    await self.do(worker.build(ASSIMILATOR, vesp))
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, StevenBot()),
