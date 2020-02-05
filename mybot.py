da5d93905a81018a7358fbfc18d58f6cea392242
stsan9 <stsan@ucsd.edu>
2020-02-04 23:07:15 -0800

    beats medium, loses to hard; no optimization of strategy yet

--- mybot.py
+++ mybot.py
@@ -1,7 +1,9 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
+    CYBERNETICSCORE, STALKER
+import random
 
 
 class StevenBot(sc2.BotAI):
@@ -11,6 +13,9 @@ class StevenBot(sc2.BotAI):
         await self.build_pylons()
         await self.build_assimilators()
         await self.expand()
+        await self.build_offensive_buildings()
+        await self.build_offensive_army()
+        await self.attack()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -40,8 +45,44 @@ class StevenBot(sc2.BotAI):
         if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
             await self.expand_now()
 
+    async def build_offensive_buildings(self):
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+
+            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+            elif len(self.units(GATEWAY)) < 3:
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+                    await self.build(GATEWAY, near=pylon)
+
+    async def build_offensive_army(self):
+        for gw in self.units(GATEWAY).ready.noqueue:
+            if self.can_afford(STALKER) and self.supply_left > 1:
+                await self.do(gw.train(STALKER))
+
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        if self.units(STALKER).amount > 15:
+            for s in self.units(STALKER).idle:
+                await self.do(s.attack(self.find_target(self.state)))
+
+        if self.units(STALKER).amount > 3:
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(self.known_enemy_units)))
+
+
+
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, StevenBot()),
-    Computer(Race.Terran, Difficulty.Easy)
+    Computer(Race.Terran, Difficulty.Hard)
     ], realtime=False)
