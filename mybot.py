ce9bbbf6f8f07732f6c2cba3d77a7934d3397570
stsan9 <stsan@ucsd.edu>
2020-02-05 17:52:01 -0800

    has a chance of beating hard bot with rules

--- mybot.py
+++ mybot.py
@@ -2,12 +2,18 @@ import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
-    CYBERNETICSCORE, STALKER
+    CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
 import random
 
 
 class StevenBot(sc2.BotAI):
+
+    def __init__(self):
+        self.IPM = 165
+        self.MAX_WORKERS = 70
+
     async def on_step(self, iteration):
+        self.iteration = iteration
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
@@ -18,9 +24,11 @@ class StevenBot(sc2.BotAI):
         await self.attack()
 
     async def build_workers(self):
-        for nexus in self.units(NEXUS).ready.noqueue:
-            if self.can_afford(PROBE):
-                await self.do(nexus.train(PROBE))
+        if len(self.units(NEXUS)) * 16 > len(self.units(PROBE)):
+            if len(self.units(PROBE)) < self.MAX_WORKERS:
+                for nexus in self.units(NEXUS).ready.noqueue:
+                    if self.can_afford(PROBE) and len(self.units(PROBE)):
+                        await self.do(nexus.train(PROBE))
 
     async def build_pylons(self):
         if self.supply_left < 5 and not self.already_pending(PYLON):
@@ -42,7 +50,7 @@ class StevenBot(sc2.BotAI):
                     await self.do(worker.build(ASSIMILATOR, vesp))
 
     async def expand(self):
-        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+        if self.units(NEXUS).amount < (self.iteration / self.IPM) and self.can_afford(NEXUS):
             await self.expand_now()
 
     async def build_offensive_buildings(self):
@@ -52,14 +60,25 @@ class StevenBot(sc2.BotAI):
             if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                     await self.build(CYBERNETICSCORE, near=pylon)
-            elif len(self.units(GATEWAY)) < 3:
+
+            elif len(self.units(GATEWAY)) < ((self.iteration / self.IPM)/2):
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
 
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(STARGATE)) < ((self.iteration / self.IPM)/2):
+                    if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
+                        await self.build(STARGATE, near=pylon)
+
     async def build_offensive_army(self):
         for gw in self.units(GATEWAY).ready.noqueue:
-            if self.can_afford(STALKER) and self.supply_left > 1:
-                await self.do(gw.train(STALKER))
+            if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
+                if self.can_afford(STALKER) and self.supply_left > 0:
+                    await self.do(gw.train(STALKER))
+
+        for sg in self.units(STARGATE).ready.noqueue:
+            if self.can_afford(VOIDRAY) and self.supply_left > 0:
+                await self.do(sg.train(VOIDRAY))
 
     def find_target(self, state):
         if len(self.known_enemy_units) > 0:
@@ -70,14 +89,20 @@ class StevenBot(sc2.BotAI):
             return self.enemy_start_locations[0]
 
     async def attack(self):
-        if self.units(STALKER).amount > 15:
-            for s in self.units(STALKER).idle:
-                await self.do(s.attack(self.find_target(self.state)))
+        # {UNIT: [n to fight, n to defend]}
+        aggressive_units = {STALKER: [15, 5],
+                            VOIDRAY: [8, 3]}
 
-        if self.units(STALKER).amount > 3:
-            if len(self.known_enemy_units) > 0:
-                for s in self.units(STALKER).idle:
-                    await self.do(s.attack(random.choice(self.known_enemy_units)))
+
+        for UNIT in aggressive_units:
+            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+
+            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 
